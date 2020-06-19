149ab027513ecdc0b58e203bbcce995500e8f0a8
delila <delila@sinitic.ai>
2020-06-19 15:49:36 +0800

    Implement transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Transaction({ transaction }) {
   const sign = transaction.amount < 0 ? "-" : "+";
+  const { deleteTransaction } = useContext(GlobalContext);
 
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
-      {transaction.text}{" "}
+      {transaction.text}
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 }
