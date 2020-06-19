08c986f25875b318890acf5de0d01dbfec272ad8
delila <delila@sinitic.ai>
2020-06-19 16:20:53 +0800

    Implement addTransaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount, // Number
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <React.Fragment>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
             type="text"
             placeholder="Enter text..."
             value={text}
-            onChange={() => setText((e) => e.target.value)}
+            onChange={(e) => setText(e.target.value)}
           />
         </div>
         <div className="form-control">
@@ -25,7 +40,7 @@ export const AddTransaction = () => {
             type="number"
             placeholder="Enter amount..."
             value={amount}
-            onChange={(e) => e.target.value}
+            onChange={(e) => setAmount(e.target.value)}
           />
         </div>
         <button className="btn">Add transaction</button>
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,8 +3,8 @@ import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
-  const amount = transactions.map((transaction) => transaction.amount);
-  const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <React.Fragment>
       <h4>Your Balance</h4>
